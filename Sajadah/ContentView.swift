30b978d21e9fcc569cc51ac5285b9754c9a98608
shaheem-pp <shanofficial2000@gmail.com>
2026-01-04 21:31:45 -0500

    Initial Commit

---
+++ Sajadah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sajadah
+//
+//  Created by Shaheem on 2026-01-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sajadah/SajadahApp.swift
@@ -0,0 +1,17 @@
+//
+//  SajadahApp.swift
+//  Sajadah
+//
+//  Created by Shaheem on 2026-01-04.
+//
+
+import SwiftUI
+
+@main
+struct SajadahApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
